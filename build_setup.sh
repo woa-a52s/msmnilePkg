70a6b8e8bfbde40c38553e2ab06438271c1849c9
KancyJoe <kancy2333@outlook.com>
2024-12-15 07:29:05 +0000

    build_setup: Fix nuget installation in ubuntu 24.04

--- build_setup.sh
+++ build_setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Assume building on ubuntu 24.04
 sudo apt update
-sudo apt install -y uuid-dev clang llvm gcc-aarch64-linux-gnu lld
 
 # Install Dependencies
-sudo apt -y install python3-venv pip git mono-devel build-essential nuget build-essential iasl nasm python3 python3-distutils python3-git python3-pip gettext locales gnupg ca-certificates python3-venv git git-core curl
+sudo apt -y install uuid-dev clang llvm gcc-aarch64-linux-gnu lld python3-venv pip git mono-devel build-essential iasl nasm python3 python3-git python3-pip gettext locales gnupg ca-certificates python3-venv git-core curl
+
+# Install nuget
+sudo curl -o /usr/local/bin/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+alias nuget="mono /usr/local/bin/nuget.exe"
 
 #cargo install --force cargo-make
 #cargo add cargo-tarpaulin
